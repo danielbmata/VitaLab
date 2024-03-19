7e7d6b2f6816abf06e2d1da4f9a655acd6f8e2c6
Daniel <d7mata@gmail.com>
2024-03-19 11:51:32 -0300

    finalizado messagens do cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.messages import constants
 from django.http import HttpResponse
 
 
 def cadastro(request):
     if request.method == 'GET':
         return render(request, 'cadastro.html')
+    
     elif request.method == 'POST':
         primeiro_nome = request.POST.get('primeiro_nome')
         ultimo_nome = request.POST.get('ultimo_nome')
@@ -15,15 +17,18 @@ def cadastro(request):
         email = request.POST.get('email')
         confirmar_senha = request.POST.get('confirmar_senha')
         
-        if not senha == confirmar_senha:
-            return redirect('/usuarios/cadastro')
-        
-        if len(senha) < 6:
-            return redirect('/usuario/cadastro')
-        
         if User.objects.filter(username=username).exists():
             messages.error(request, 'Este nome de usuário já está em uso. Por favor, escolha outro.')
             return redirect('/usuarios/cadastro') 
+        
+        if not senha == confirmar_senha:
+            messages.error(request, 'As senhas não coincidem.')
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            messages.error(request, 'A senha deve conter 7 ou mais caracteres.')
+            return redirect('/usuarios/cadastro')
+        
         
         try:
             user = User.objects.create_user(
@@ -32,8 +37,12 @@ def cadastro(request):
                 username = username,
                 password = senha,
                 email = email,
-                )
+            )
+            messages.success(request,'Usuário cadastrado com sucesso!')
+            
         except:
+            messages.error(request,'Erro interno do sistema, contate um administrador.')
+            
             return redirect('/usuarios/cadastro')
         
         return redirect('/usuarios/cadastro')
