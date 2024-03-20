e0bdbaf07f0c215492a806f84deb78789022d88e
Daniel <d7mata@gmail.com>
2024-03-20 09:49:59 -0300

    models de exames

--- exames/models.py
+++ exames/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class TiposExames(models.Model):
     tipo_choices = (
@@ -11,7 +12,32 @@ class TiposExames(models.Model):
     preco = models.FloatField()
     disponivel = models.BooleanField(default=True)
     horario_inicial = models.IntegerField()
-    horario_final = models.ImageField()
+    horario_final = models.IntegerField()
     
     def __str__(self):
         return self.nome
+    
+class SolicitacaoExame(models.Model):
+    choice_status = (
+        ('E', 'Em análise'),
+        ('F', 'Finalizado')
+    )
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    exame = models.ForeignKey(TiposExames, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=2, choices=choice_status)
+    resultado = models.FileField(upload_to="resultados", null=True, blank=True)
+    requer_senha = models.BooleanField(default=False)
+    senha = models.CharField(max_length=6, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.usuario} | {self.exame.nome}'
+    
+class PedidosExames(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    exames = models.ManyToManyField(SolicitacaoExame)
+    agendado = models.BooleanField(default=True)
+    data = models.DateField()
+
+    def __str__(self):
+        return f'{self.usuario} | {self.data}'
+    
